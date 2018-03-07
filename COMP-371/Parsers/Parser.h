e1bb43778005f55725afe1f4b3b398b607cb0ec7
Christophe Savard <christophe_savard@hotmail.ca>
2018-03-07 17:20:35 -0500

    Moved template definition to header

--- COMP-371/Parsers/Parser.h
+++ COMP-371/Parsers/Parser.h
@@ -36,3 +36,19 @@ protected:
 	/// \brief Parser file location
 	const std::string location;
 };
+
+//Template definitions
+template <class T>
+Parser<T>::Parser() : location("") { }
+
+template <class T>
+Parser<T>::Parser(const std::string location) : location(location) { }
+
+template <class T>
+Parser<T>::~Parser() { }
+
+template <class T>
+T Parser<T>::parseAll() const
+{
+	throw std::runtime_error("No parser available for this type");
+}
